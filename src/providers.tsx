37ec185fd0e20a3a7d910e9ff98873c5370a5283
mxvsh <hi@monawwar.io>
2024-10-16 13:09:10 +0530

    chore: setup providers

---
+++ src/providers.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React from 'react';
+
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+import { ThemeProvider } from 'next-themes';
+import { Toaster } from 'sonner';
+
+type Props = {
+  children: React.ReactNode;
+};
+function Providers({ children }: Props) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      disableTransitionOnChange
+    >
+      <Toaster position="bottom-center" />
+      {children}
+      <ProgressBar
+        height="3px"
+        color="#5d6eee"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+    </ThemeProvider>
+  );
+}
+
+export default Providers;
