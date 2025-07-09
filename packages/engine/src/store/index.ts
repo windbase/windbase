11dd8a570e98d3fefebf4caaf60b2285e9818121
mxvsh <xencodes@icloud.com>
2025-07-09 11:44:21 +0530

    feat: add persistence to builder store using zustand middleware

--- packages/engine/src/store/index.ts
+++ packages/engine/src/store/index.ts
@@ -1,16 +1,24 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { createHistorySlice } from '../history';
 import { createManipulationSlice } from '../manipulation';
 import { createSelectionSlice } from '../selection';
 import { createTemplateSlice } from '../template';
 import type { BuilderStore } from './types';
 
-export const useBuilder = create<BuilderStore>()((...a) => ({
-	...createHistorySlice(...a),
-	...createSelectionSlice(...a),
-	...createManipulationSlice(...a),
-	...createTemplateSlice(...a)
-}));
+export const useBuilder = create<BuilderStore>()(
+	persist(
+		(...a) => ({
+			...createHistorySlice(...a),
+			...createSelectionSlice(...a),
+			...createManipulationSlice(...a),
+			...createTemplateSlice(...a)
+		}),
+		{
+			name: 'builder-store'
+		}
+	)
+);
 
 // Export types for external usage
 export type {
