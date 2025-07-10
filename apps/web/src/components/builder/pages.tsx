9299643bc1364ab47cd7d4aa23632dd0a92f07c4
mxvsh <xencodes@icloud.com>
2025-07-10 11:55:48 +0530

    feat: support dynamic page creation and improved layout

--- apps/web/src/components/builder/pages.tsx
+++ apps/web/src/components/builder/pages.tsx
@@ -1,16 +1,46 @@
-import { Book, Ellipsis } from 'lucide-react';
+import { useBuilder } from '@windbase/engine';
+import { Book, Ellipsis, Plus } from 'lucide-react';
+import { useInputDialog } from '@/lib/hooks/use-input-dialog';
 
 function Pages() {
+	const { pages, getCurrentPage, setCurrentPage, createPage } = useBuilder();
+
+	const currentPage = getCurrentPage();
+
+	const { ask } = useInputDialog();
+
 	return (
 		<>
-			<h1 className="sticky top-0 z-10 text-xs font-medium px-3 py-3 uppercase text-muted-foreground flex items-center gap-2">
-				<Book size={16} /> Pages
-			</h1>
-			<div className="p-2 space-y-2">
-				<div className="flex items-center justify-between gap-2 cursor-pointer py-1.5 px-3 rounded-sm hover:bg-muted">
-					<p className="text-sm font-medium">index</p>
-					<Ellipsis size={16} />
-				</div>
+			<div className="flex justify-between items-center sticky top-0 z-10 bg-background px-3 py-3">
+				<h1 className="text-xs font-medium uppercase text-muted-foreground flex items-center gap-2">
+					<Book size={16} /> Pages
+				</h1>
+				<Plus
+					size={16}
+					className="text-muted-foreground hover:text-foreground cursor-pointer"
+					onClick={() => {
+						ask('New Page', 'Enter the name of the new page').then((name) => {
+							if (name) {
+								createPage(name);
+							}
+						});
+					}}
+				/>
+			</div>
+			<div className="p-2">
+				{Array.from(pages?.values() || []).map((page) => (
+					<button
+						key={page.id}
+						type="button"
+						className={`group w-full flex items-center justify-between gap-2 py-1.5 px-3 rounded-sm ${
+							currentPage?.id === page.id ? 'bg-muted' : ''
+						}`}
+						onClick={() => setCurrentPage(page.id)}
+					>
+						<p className="text-sm">{page.name}</p>
+						<Ellipsis size={16} className="hidden group-hover:block" />
+					</button>
+				))}
 			</div>
 		</>
 	);
