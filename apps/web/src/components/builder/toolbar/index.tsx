859967f15e9edcc226dc8eb17ab582a2e2286f79
mxvsh <xencodes@icloud.com>
2025-07-17 00:45:54 +0530

    fix: move tailwind from menu to toolbar

--- apps/web/src/components/builder/toolbar/index.tsx
+++ apps/web/src/components/builder/toolbar/index.tsx
@@ -7,23 +7,28 @@ import {
 	TooltipProvider,
 	TooltipTrigger
 } from '@windbase/ui';
-import { Eye, Redo, Undo } from 'lucide-react';
+import { Eye, Palette, Redo, Undo } from 'lucide-react';
+import { useState } from 'react';
 import ElementPicker from '../../elements/picker';
 import BlockPicker from '../block-picker';
 import TemplatePicker from '../template-picker';
 import DarkModeButton from './dark-mode-button';
 import ExportButton from './export-button';
 import ToolbarMenu from './menu';
+import NewReleaseIndicator from './release-indicator';
+import TailwindCSSConfig from './tailwindcss-config';
 import ViewCodeButton from './view-code';
 
 function Toolbar() {
+	const [viewTailwindCSSConfig, setViewTailwindCSSConfig] = useState(false);
 	const { undo, redo, canUndo, canRedo, exportHtml } = useBuilder();
 
 	return (
 		<TooltipProvider>
 			<div className="h-14 min-h-14 border-b grid grid-cols-8 justify-between items-center px-3 gap-4">
-				<div className="col-span-3 flex justify-start">
+				<div className="col-span-3 flex items-center gap-3 justify-start">
 					<ToolbarMenu />
+					<NewReleaseIndicator />
 				</div>
 				<div className="mx-auto flex items-center col-span-2 gap-2"></div>
 
@@ -31,6 +36,20 @@ function Toolbar() {
 					<ElementPicker />
 					<BlockPicker />
 					<TemplatePicker />
+					<Tooltip delayDuration={200}>
+						<TooltipTrigger asChild>
+							<Button
+								variant="outline"
+								size="icon"
+								onClick={() => setViewTailwindCSSConfig(true)}
+							>
+								<Palette />
+							</Button>
+						</TooltipTrigger>
+						<TooltipContent>
+							<p>Theme Config</p>
+						</TooltipContent>
+					</Tooltip>
 
 					<Separator orientation="vertical" className="h-4 mx-2" />
 
@@ -104,6 +123,11 @@ function Toolbar() {
 					</Tooltip>
 				</div>
 			</div>
+
+			<TailwindCSSConfig
+				open={viewTailwindCSSConfig}
+				setOpen={setViewTailwindCSSConfig}
+			/>
 		</TooltipProvider>
 	);
 }
