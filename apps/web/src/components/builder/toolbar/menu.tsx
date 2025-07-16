5c6c464b30e71db03efbe101380a3fd81956ea87
mxvsh <xencodes@icloud.com>
2025-07-17 00:46:09 +0530

    feat: rotating logo on hover

--- apps/web/src/components/builder/toolbar/menu.tsx
+++ apps/web/src/components/builder/toolbar/menu.tsx
@@ -6,61 +6,76 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger
 } from '@windbase/ui';
-import { ChevronDown } from 'lucide-react';
-import { useState } from 'react';
-import TailwindCSSConfig from './tailwindcss-config';
+import { ChevronDown, Globe } from 'lucide-react';
+import { useEffect, useRef } from 'react';
+
+let rotation = 0;
 
 function ToolbarMenu() {
-	const [viewTailwindCSSConfig, setViewTailwindCSSConfig] = useState(false);
-	return (
-		<>
-			<DropdownMenu>
-				<DropdownMenuTrigger asChild>
-					<div className="flex items-center gap-2">
-						<img
-							src="/windbase-circle.svg"
-							alt="Windbase"
-							width={100}
-							height={100}
-							className="h-8 w-8 rounded-md"
-						/>
-						<div className="px-2 py-1 bg-muted rounded-xl text-xs pointer-events-none">
-							v{VERSION}
-						</div>
-						<ChevronDown size={16} />
-					</div>
-				</DropdownMenuTrigger>
-				<DropdownMenuContent align="start" className="w-56">
-					<DropdownMenuLabel>Windbase</DropdownMenuLabel>
-					<DropdownMenuSeparator />
-					<DropdownMenuItem>New Project</DropdownMenuItem>
-					<DropdownMenuItem>Open Project</DropdownMenuItem>
-					<DropdownMenuSeparator />
-					<DropdownMenuItem onClick={() => setViewTailwindCSSConfig(true)}>
-						TailwindCSS Config
-					</DropdownMenuItem>
-					<DropdownMenuSeparator />
-					<DropdownMenuItem
-						className="text-destructive"
-						onClick={() => {
-							// Confirm the action
-							if (!confirm('Are you sure you want to delete this project?'))
-								return;
+	const imageRef = useRef<HTMLImageElement>(null);
 
-							// Delete the project
-							window.localStorage.removeItem('builder-store');
-							window.location.reload();
-						}}
-					>
-						Delete Project
-					</DropdownMenuItem>
-				</DropdownMenuContent>
-			</DropdownMenu>
-			<TailwindCSSConfig
-				open={viewTailwindCSSConfig}
-				setOpen={setViewTailwindCSSConfig}
-			/>
-		</>
+	useEffect(() => {
+		let interval: NodeJS.Timeout;
+		if (imageRef.current) {
+			const image = imageRef.current;
+			image.addEventListener('mouseenter', () => {
+				interval = setInterval(() => {
+					image.style.transform = `rotate(${rotation}deg)`;
+					rotation += 10;
+				}, 100);
+			});
+
+			image.addEventListener('mouseleave', () => {
+				clearInterval(interval);
+			});
+		}
+	}, []);
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<div className="flex items-center gap-2">
+					<img
+						ref={imageRef}
+						src="/windbase-circle.svg"
+						alt="Windbase"
+						width={100}
+						height={100}
+						draggable={false}
+						className="h-8 w-8 rounded-md"
+					/>
+					<div className="px-2 py-1 bg-muted rounded-xl text-xs pointer-events-none">
+						v{VERSION}
+					</div>
+					<ChevronDown size={16} />
+				</div>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="start" className="w-56">
+				<DropdownMenuLabel>Windbase</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem>New Project</DropdownMenuItem>
+				<DropdownMenuItem>Open Project</DropdownMenuItem>
+				<DropdownMenuItem
+					className="text-destructive"
+					onClick={() => {
+						// Confirm the action
+						if (!confirm('Are you sure you want to delete this project?'))
+							return;
+
+						// Delete the project
+						window.localStorage.removeItem('builder-store');
+						window.location.reload();
+					}}
+				>
+					Delete Project
+				</DropdownMenuItem>
+				<DropdownMenuSeparator />
+				<DropdownMenuLabel>Links</DropdownMenuLabel>
+				<DropdownMenuItem onClick={() => window.open('https://github.com/windbase/windbase', '_blank')}><span className="icon-[simple-icons--github]"></span> GitHub</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => window.open('https://discord.gg/wHMpedAzhT', '_blank')}><span className="icon-[simple-icons--discord]"></span> Discord</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => window.open('https://windbase.dev', '_blank')}><Globe /> Website</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
 	);
 }
 
