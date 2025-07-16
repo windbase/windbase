1eaa459fe9b4d12e226344744ec066ee08420cef
mxvsh <xencodes@icloud.com>
2025-07-16 23:24:13 +0530

    fix: set default tailwindcss config theme

--- packages/engine/src/global-config/index.ts
+++ packages/engine/src/global-config/index.ts
@@ -7,6 +7,24 @@ export const createGlobalConfigSlice: StateCreator<
 	[],
 	GlobalConfigSlice
 > = (set) => ({
-	tailwindCSSConfig: '',
+	tailwindCSSConfig: `@import "tailwindcss";
+
+/* Optional: Dark mode variant using class strategy */
+@custom-variant dark (&:is(.dark *));
+
+:root {
+  --primary: oklch(0.6962 0.1628 241.41);
+  --primary-foreground: oklch(0.985 0 0);
+}
+
+.dark {
+  --primary: oklch(0.985 0 0);
+  --primary-foreground: oklch(0.205 0 0);
+}
+
+@theme inline {
+  --color-primary: var(--primary);
+  --color-primary-foreground: var(--primary-foreground);
+}`,
 	setTailwindCSSConfig: (config: string) => set({ tailwindCSSConfig: config })
 });
